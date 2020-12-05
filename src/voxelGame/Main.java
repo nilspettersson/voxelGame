4e936bc8e1646c0b7e2111531d817893d3112c91
Nils Pettersson <nils-pettsson@outlook.com>
2020-12-05 11:07:37 +0100

    init

---
+++ src/voxelGame/Main.java
@@ -0,0 +1,17 @@
+package voxelGame;
+
+import niles.lwjgl.loop.Game;
+
+public class Main extends Game{
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
